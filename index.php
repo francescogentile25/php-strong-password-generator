071ac045838b67e7b0ed6acab9e1ad79a2a3a902
francescogentile25 <francescogentile250893@gmail.com>
2023-03-21 14:51:54 +0100

    FEAT add form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Ciao</title>
+</head>
+
+<body>
+    <h1>Ciao</h1>
+    <div class="container">
+        <form action="index.php" method="GET">
+            <input type="text" name="password" placeholder="inserisci la lunghezza della password" class="w-25">
+            <button type="submit">Invia</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
